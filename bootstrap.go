db129789219f5528636675abe65d853256db5b65
zhangfumu <53540168@qq.com>
2017-06-04 10:43:19 +0800

    新增加启动命令选项

---
+++ bootstrap.go
@@ -0,0 +1,85 @@
+package bingo
+
+import(
+    "flag"
+    "github.com/armson/bingo/config"
+    "os"
+    "fmt"
+    "path/filepath"
+    "strconv"
+)
+
+var usageStr = `Usage:%s [options]
+Server Options:
+    -c, --config <file>              Configuration file path
+Common Options:
+    -h, --help                       Show this message
+    -v, --version                    Show version
+`
+func usage(){
+    fmt.Printf(usageStr, os.Args[0])
+    os.Exit(0)
+}
+
+func createPIDFile() error {
+    pidPath, err := config.String("pid")
+    if err != nil {
+        return err
+    }
+     _, er := os.Stat(pidPath)
+     pidOverride := true
+    if os.IsNotExist(er) || pidOverride == true {
+        currentPid := os.Getpid()
+        if err := os.MkdirAll(filepath.Dir(pidPath), os.ModePerm); err != nil {
+            return fmt.Errorf("Can't create PID folder on %v", err)
+        }
+
+        file, err := os.Create(pidPath)
+        if err != nil {
+            return fmt.Errorf("Can't create PID file: %v", err)
+        }
+        defer file.Close()
+        if _, err := file.WriteString(strconv.FormatInt(int64(currentPid), 10)); err != nil {
+            return fmt.Errorf("Can'write PID information on %s: %v", pidPath, err)
+        }
+    } else {
+        return fmt.Errorf("%s already exists", pidPath)
+    }
+    return nil
+}
+
+func init(){
+    var isShowVersion bool
+    var configFile string
+    flag.StringVar(&configFile, "c", "conf/app.conf", "Configuration file path.")
+    flag.StringVar(&configFile, "config", "conf/app.conf", "Configuration file path.")
+    flag.BoolVar(&isShowVersion, "version", false, "Print version information.")
+    flag.BoolVar(&isShowVersion, "v", false, "Print version information.")
+    flag.Usage = usage
+    flag.Parse()
+
+    args := os.Args
+    if len(args) < 2 { 
+        usage() 
+    }
+    if isShowVersion {
+        PrintVersion()
+        os.Exit(0)  
+    }
+       
+    err := config.Load(configFile)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(0)
+    }
+
+    if err = createPIDFile(); err != nil {
+        fmt.Println(err)
+        os.Exit(0)
+    }
+}
+
+
+
+
+
